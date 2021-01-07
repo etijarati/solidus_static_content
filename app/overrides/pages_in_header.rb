5c09b60c16898df8ab5529637a130be81b8f7b88
Gharbi Mohammed <mohammed@staffomatic.com>
2021-01-07 21:48:21 +0100

    adjust with template

--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :name => "pages_in_header",
-                     :insert_bottom => "#main-nav-bar",
+                     :insert_before => "#main-nav-bar-contact",
                      :partial => "spree/static_content/static_content_header",
                      :disabled => false)
